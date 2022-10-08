707c6050a8214cd030e74079fe3c33bf7ff06953
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-10-09 00:14:09 +0200

    deer et leaddeer

--- Assets/Scripts/Deers/DeersGroup.cs
+++ Assets/Scripts/Deers/DeersGroup.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DeersGroup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    NavMeshAgent agent;
+    private void Awake()
     {
-        
+        DeersPatrol.VectorToDestination += GoToo;
+        DeersPatrol.Stop += OnStop;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void OnDestroy()
+    {
+        DeersPatrol.VectorToDestination -= GoToo;
+        DeersPatrol.Stop -= OnStop;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void GoToo(Vector3 target)
+    {
+        agent.SetDestination(target);
+    }
+
+    private void OnStop()
+    {
+        agent.isStopped = true;
+        agent.ResetPath();
     }
 }
